6ce7d62e0bc8aec1d1366257b4b506e65f96fb38
wirigiwewe <addict.hamster@gmail,com>
2022-10-04 18:26:43 +0600

    filter

--- src/main/java/com/example/project/GreetingController.java
+++ src/main/java/com/example/project/GreetingController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -16,35 +17,32 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
 
     @GetMapping
     public String main(Model model, @ModelAttribute("user") User user) {
-       Iterable<User> users = userRepo.findAll();
+        Iterable<User> users = userRepo.findAll();
         model.addAttribute("users", users);
         return "main";
     }
-   @PostMapping
-    public String add(@ModelAttribute("user") User user, Model model){
+
+    @PostMapping
+    public String add(@ModelAttribute("user") User user, Model model) {
         userRepo.save(user);
 
         Iterable<User> users = userRepo.findAll();
         model.addAttribute("users", users);
         return "main";
     }
-/* @PostMapping()
- public @ResponseBody String addNewUser (@RequestParam(name="email", required=false, defaultValue="email") String email,
-                                         @RequestParam(name="login", required=false, defaultValue="login") String login,Model model) {
-     // @ResponseBody means the returned String is the response, not a view name
-     // @RequestParam means it is a parameter from the GET or POST request
-     User n = new User();
-     n.setLogin(login);
-     n.setEmail(email);
-     userRepo.save(n);
-     return "Saved";
- }*/
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Model model,  @ModelAttribute("user") User user) {
+        List<User> users = userRepo.findByCity(filter);
+        model.addAttribute("users", users);
+        return "main";
+    }
 
 }
--- src/main/java/com/example/project/repos/UserRepo.java
+++ src/main/java/com/example/project/repos/UserRepo.java
@@ -3,5 +3,8 @@ package com.example.project.repos;
 import com.example.project.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepo extends CrudRepository<User, Long> {
+    List<User> findByCity(String city);
 }
